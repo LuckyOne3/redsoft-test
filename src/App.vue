2bfc89f8950eabc759e0d789fb29b15d78c6decc
Алексей <alex@loc.local>
2021-01-30 03:58:23 +0300

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,231 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <!-- eslint-disable max-len -->
+  <div id="app" class="d-flex column space-between">
+    <div class="nav__and__views">
+      <div class="nav">
+        <div class="nav__container container d-flex row align-center space-between">
+          <div class="nav__container__left d-flex row align-center space-between ">
+            <img src="./assets/logoIcons/logo-museum 2.svg">
+            <router-link class="h4" to="/">Каталог</router-link>
+            <router-link class="h4" to="/about">Доставка</router-link>
+            <router-link class="h4" to="/about">Оплата</router-link>
+            <router-link class="h4" to="/about">Контакты</router-link>
+            <router-link class="h4" to="/about">О галерее</router-link>
+          </div>
+          <div class="nav__container__right d-flex row align-center space-between">
+            <div class="nav__container__right__inner d-flex row align-center space-around">
+              <div class="nav__container__right__inner__form h6 text-left">
+                Поиск по названию картины
+              </div>
+              <div class="nav__container__right__inner__find h4">
+                Найти
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="views">
+        <div class="views__container container">
+          <router-view/>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="footer">
+      <div class="footer__nav nav">
+        <div class="nav__container container d-flex row align-center space-between">
+          <div class="nav__container__left d-flex row align-center space-between ">
+            <img src="./assets/logoIcons/logo-museum-next.svg">
+            <router-link class="h4" to="/">Каталог</router-link>
+            <router-link class="h4" to="/about">Доставка</router-link>
+            <router-link class="h4" to="/about">Оплата</router-link>
+            <router-link class="h4" to="/about">Контакты</router-link>
+            <router-link class="h4" to="/about">О галерее</router-link>
+          </div>
+          <div class="nav__container__right footer__phone__and__address d-flex row align-center space-between">
+            <div class="nav__container__right__inner w100 n-brd d-flex row align-center space-between">
+              <div class="nav__container__right__inner__phone h5 text-left  d-flex align-center">
+                <img class="nav__container__right__inner__img" src="./assets/logoIcons/phone.svg">
+                +7 (495) 555-55-55
+              </div>
+              <div  class="nav__container__right__inner__address h5 d-flex align-center">
+                <img class="nav__container__right__inner__img" src="./assets/logoIcons/geo.svg">
+                г. Москва, ул. Расплетина, 24
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Merriweather', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #F6F3F3;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
+.nav {
+
+  border-bottom: 1px solid #E1E1E1;
+  height: 95px;
+  padding: 0px;
+  margin: 0px;
 
   a {
-    font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+
+.nav__container {
+  height: 100%;
+}
+
+.nav__container__left {
+  width: 50.6%;
+}
+
+.nav__container__right__inner {
+  border: 1px solid #E1E1E1;
+  height: 48px;
+}
+
+.nav__container__right__inner__form {
+  width: 276px;
+  color: #9F9F9F;
+  margin-left: 16px;
+}
+
+.nav__container__right__inner__find {
+  color: white;
+  padding-left: 36px;
+  padding-right: 36px;
+  padding-top: 13px;
+  font-weight: bold;
+  padding-bottom: 14px;
+  background: #403432;
+}
+
+.footer {
+  background-color: #ECEAEA;
+  height: 93px;
+  a{
+    color: #555555;
+  }
+}
+.footer__phone__and__address{
+  width: 446px;
+}
+.nav__container__right__inner__img{
+  margin-right: 8px;
+}
+
+.h1 {
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 36px;
+}
+
+.h2 {
+  font-size: 18px;
+  font-weight: normal;
+  line-height: 24px;
+}
+
+.h3 {
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 24px;
+}
+
+.h4 {
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 21px;
+}
+
+.h5 {
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 21px;
+}
+
+.h6 {
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 21px;
+}
+
+.w100 {
+  width: 100%;
+}
+
+.w50 {
+  width: 50%;
+}
+
+.w30 {
+  width: 30%;
+}
+
+.container {
+  position: relative;
+  display: block;
+  width: 1216px;
+  margin: auto;
+
+}
+
+.mt-4 {
+  margin-top: 46px;
+}
+
+.mb-2 {
+  margin-bottom: 39px;
+}
+
+.pb-2 {
+  padding-bottom: 24px;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.row {
+  flex-direction: row;
+}
+
+.column {
+  flex-direction: column;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.text-left {
+  text-align: left;
+}
+
+.align-center {
+  align-items: center;
+}
+
+.space-around {
+  justify-content: space-around;
+}
+
+.space-between {
+  justify-content: space-between;
+}
+.n-brd{
+  border: none!important;
+}
+
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="pictures">
+    <div class="pictures__title h1 text-left mt-4 mb-2">
+      Картины эпохи Возрождения
+    </div>
+    <div class="pictures__inner d-flex row">
+      <div class="picture pb-2">
+        <div class="picture__body d-flex column">
+          <div class="picture__body__img">
+            <img src="../assets/pictures/venera.svg">
+          </div>
+          <div class="picture__body__title h2 text-left">
+            «Рождение Венеры»
+          </div>
+          <div class="picture__body__author h2 text-left">
+            Сандро Боттичелли
+          </div>
+          <div class="picture__body__buy__and__price d-flex row align-center space-around">
+            <div class="picture__body__prices d-flex column space-between  text-left">
+              <div class="picture__body__old__price h6 ">
+                2 000 000 $
+              </div>
+              <div class="picture__body__price h3 ">
+                1 000 000 $
+              </div>
+            </div>
+            <div class="picture__body__buy button_buy h5 d-flex align-center">
+              Купить
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="picture pb-2">
+        <div class="picture__body d-flex column">
+          <div class="picture__body__img">
+            <img src="../assets/pictures/ve4er.svg">
+          </div>
+          <div class="picture__body__title h2 text-left">
+            «Тайная вечеря»
+          </div>
+          <div class="picture__body__author h2 text-left">
+            Леонардо да Винчи
+          </div>
+          <div class="picture__body__buy__and__price d-flex row align-center space-around">
+            <div class="picture__body__prices d-flex column space-between  text-left">
+              <div class="picture__body__old__price h6 ">
+              </div>
+              <div class="picture__body__price h3 ">
+                3 000 000 $
+              </div>
+            </div>
+            <div class="picture__body__buy button_buy h5 d-flex align-center">
+              Купить
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="picture pb-2">
+        <div class="picture__body d-flex column">
+          <div class="picture__body__img">
+            <img src="../assets/pictures/adam.svg">
+          </div>
+          <div class="picture__body__title h2 text-left">
+            «Сотворение Адама»
+          </div>
+          <div class="picture__body__author h2 text-left">
+            Микеланджело
+          </div>
+          <div class="picture__body__buy__and__price d-flex row align-center space-around">
+            <div class="picture__body__prices d-flex column space-between  text-left">
+              <div class="picture__body__old__price h6 ">
+                6 000 000 $
+              </div>
+              <div class="picture__body__price h3 ">
+                5 000 000 $
+              </div>
+            </div>
+            <div class="picture__body__buy button_buy button_buy_in_cart h5 d-flex align-center">
+              В корзине
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="picture picture__bought pb-2">
+        <div class="picture__body d-flex column">
+          <div class="picture__body__img">
+            <img src="../assets/pictures/anatomia.svg">
+          </div>
+          <div class="picture__body__title h2 text-left">
+            «Урок анатомии»
+          </div>
+          <div class="picture__body__author h2 text-left">
+            Рембрандт
+          </div>
+          <div class="picture__body__buy__and__price d-flex row align-center ">
+            <div class="picture__body__prices d-flex column space-between  text-left">
+              <div class="picture__body__old__price h6 ">
+
+              </div>
+              <div class="picture__body__price price__bought h3 ">
+                Продана на аукционе
+              </div>
+            </div>
+            <div class="picture__body__buy button_buy button_buy__bought h5 d-flex align-center">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'mainPage',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.picture {
+  width: 280px;
+  position: relative;
+  border: 1px solid #E1E1E1;
+  margin-right: 32px;
+
+  &:last-child {
+    margin-right: 0px;
+  }
+}
+
+.picture__body__img img {
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+  border-bottom: 1px solid #E1E1E1;
+}
+
+.picture__body__title {
+  margin-top: 20px;
+}
+
+.picture__body__author {
+  margin-top: 3px;
+}
+
+.picture__body__title, .picture__body__author {
+  margin-left: 23px;
+}
+
+.picture__body__prices {
+  margin-left: 12px;
+}
+
+.picture__body__old__price {
+  color: #A0A0A0;
+  text-decoration-line: line-through;
+
+}
+
+.picture__body__price {
+  color: #343030;
+}
+
+.picture__body__buy__and__price {
+  margin-top: 23px;
+}
+
+.picture__body__buy {
+  color: white;
+  padding-left: 31px;
+  padding-right: 31px;
+  padding-top: 13px;
+  margin-right: 12px;
+  font-weight: bold;
+  padding-bottom: 14px;
+  background: #403432;
+}
+.button_buy_in_cart{
+  background-color: #5B3A32;
+  padding-left: 10px;
+  padding-right: 11px;
+  &:before{
+    content: url('../assets/logoIcons/feather_check.svg');
+    width: 13px;
+    height: 9px;
+    margin-right: 11px;
+    margin-top: -12px;
+  }
+}
+.button_buy__bought{
+  display: none;
+}
+.price__bought{
+  text-align: left;
+  width: 100%;
+}
+.picture__bought{
+  opacity: 0.5;
+}
+
+</style>
